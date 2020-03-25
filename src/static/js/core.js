60ac25aa7c3658b07a084a3c44f7552a039cb12b
mmariano <marcusney@gmail.com>
2020-03-25 19:50:22 -0300

    Update the entire site from Bootstrap framework to Materialize framework.

---
+++ src/static/js/core.js
@@ -0,0 +1,15 @@
+// Navbar
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elemsDropdown, {
+    coverTrigger: false
+});
+const elemsSidenav = document.querySelectorAll(".sidenav");
+const instancesSidenav = M.Sidenav.init(elemsSidenav, {
+    edge: "left"
+});
+
+(function($){
+  $(function(){
+    $('.parallax').parallax();
+  });
+})(jQuery);
